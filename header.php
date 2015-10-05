55285726ed220eb1f51e16e3ce07a56fa4fbbeda
klevismiho <klevisi@gmail.com>
2015-10-05 13:29:58 +0200

    Fix header.php

--- header.php
+++ header.php
@@ -7,6 +7,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
     <script src="<?php bloginfo('template_url') ?>/bower_components/modernizr/modernizr.js"></script>
-    <?php wp_header() ?>
+    <?php wp_head() ?>
 </head>
 <body>
